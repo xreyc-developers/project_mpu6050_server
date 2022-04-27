01819e5bdddd724f82fb5aae86f688652515e416
kezeah dongon <kezeahdongon54@gmail.com>
2022-04-27 17:36:40 +0800

    second

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const app = require('express')();
 const http = require('http').Server(app);
-const io = require('socket.io')(http, { cors: { origin: "http://localhost:3000" }});
+const io = require('socket.io')(http, { cors: { origin: "http://192.168.1.241:3000" }});
 const cors = require('cors');
 
 app.use(cors());
